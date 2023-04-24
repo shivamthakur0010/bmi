3b50f68048cf6cbcd75909d5f3adcb49b5395896
shivamthakur0010 <demonking0010@gmail.com>
2023-04-25 00:56:28 +0530

    Updates

---
+++ precache-manifest.7cc8436b2bd795793ae717418ae7e7d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de6c49c42af90c7fe8d6ec7eb0c7f377",
+    "url": "/bmi/index.html"
+  },
+  {
+    "revision": "26f54e8292b51c89fecf",
+    "url": "/bmi/static/css/2.ae9e8b99.chunk.css"
+  },
+  {
+    "revision": "ccf84853afb4ef17e981",
+    "url": "/bmi/static/css/main.fce36487.chunk.css"
+  },
+  {
+    "revision": "26f54e8292b51c89fecf",
+    "url": "/bmi/static/js/2.ce518bbc.chunk.js"
+  },
+  {
+    "revision": "ccf84853afb4ef17e981",
+    "url": "/bmi/static/js/main.1c1c898b.chunk.js"
+  },
+  {
+    "revision": "e4166903decdd9e7a765",
+    "url": "/bmi/static/js/runtime~main.d6c113cc.js"
+  }
+]);
